20a29a3ef3621ee8ac61fb1548cb57e512520997
carot <some-email@domain.com>
2018-03-24 15:19:36 -0700

    Module registration

---
+++ SampleModule/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cattran
+ * Date: 3/24/18
+ * Time: 2:09 PM
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mastering_SampleModule', __DIR__);
