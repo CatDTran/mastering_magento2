ebafc4b5b398ccc0c9a6e9bea0fa43c19e9cfc68
carot <some-email@domain.com>
2018-03-24 16:07:26 -0700

    Create database install script for SampleModule

---
+++ SampleModule/Setup/InstallData.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cattran
+ * Date: 3/24/18
+ * Time: 3:31 PM
+ */
+namespace Mastering\SampleModule\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallData implements InstallDataInterface {
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->getConnection()->insert(
+            $setup->getTable('mastering_sample_item'),
+            ['name' => 'Item 1']
+        );
+        $setup->getConnection()->insert(
+            $setup->getTable('mastering_sample_item'),
+            ['name' => 'Item 2']
+        );
+
+        $setup->endSetup();
+    }
+}
