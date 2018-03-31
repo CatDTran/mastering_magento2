4650374a163934241225182db91a64997d41ce8b
carot <some-email@domain.com>
2018-03-31 14:15:02 -0500

    Create a cron job to add an item into 'mastering_sample_item'

---
+++ SampleModule/Cron/AddItem.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cattran
+ * Date: 3/31/18
+ * Time: 1:35 PM
+ */
+namespace Mastering\SampleModule\Cron;
+
+use Mastering\SampleModule\Model\ItemFactory;
+use Mastering\SampleModule\Model\Config;
+
+class AddItem {
+    private $itemFactory;
+
+    public function __construct(ItemFactory $itemFactory) {
+        $this->itemFactory = $itemFactory;
+    }
+
+    public function execute() {
+        $this->itemFactory->create()
+            ->setName('Scheduled item')
+            ->setDescription('Created at ' . time())
+            ->save();
+    }
+}
